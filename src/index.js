7503f4aae2c37d5b63627771dcde8932af3763b4
Aji Maulana <mancode700@gmail.com>
2024-11-24 12:27:28 +0000

    update big

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import http from "http";
-import app from "./app.js";
-const PORT = process.env.PORT || 8080;
-
-const server = http.createServer(app);
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
