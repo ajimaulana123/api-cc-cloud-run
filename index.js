2f32f4d3a289333ec85accf2f876ff8933719d66
Aji Maulana <mancode700@gmail.com>
2024-11-26 14:39:58 +0000

    update index.js

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ const app = require("./app");
 
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.status(200).json({
-    data: {
-      status: "success",
-      message: "Hello World!",
-    } 
-  });
-});
-
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
